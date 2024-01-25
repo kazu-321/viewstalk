9a568b43499e5339ff00e959555b6a62d4b44bc4
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-01-25 15:52:17 +0900

    Update hack.js

--- hack.js
+++ hack.js
@@ -2,4 +2,6 @@ setInterval(()=>{
   document.querySelectorAll("font").forEach(dev=>{dev.innerHTML="unchi"});
   document.querySelector("#pp>center>img").src="https://raw.githubusercontent.com/kazu-321/viewstalk/main/unchi.png";
   document.querySelector("#change").href="http://abehiroshi.la.coocan.jp/";
+  document.querySelector("body>div.main>nav>div>div>a").href="https://hackertyper.net/#";
+  document.querySelector("body>div.main>nav>div>div>a>img").src="https://raw.githubusercontent.com/kazu-321/viewstalk/main/unchi.png";
 },10)
