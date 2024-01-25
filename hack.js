899593513df2f24e5993dd5755a74801cf0b1bb3
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-01-25 15:18:34 +0900

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,4 @@
+setInterval(()=>{
+  document.querySelectorAll("font").forEach(dev=>{dev.innerHTML="unchi"});
+  document.querySelector("#pp>center>img").src="https://frame-illust.com/fi/wp-content/uploads/2018/02/unichi-makimaki.png";
+},10)
