8f72d6b8ac302f00cd061686708c23908bcce070
Kazu <83614559+kazu-321@users.noreply.github.com>
2024-01-25 15:38:06 +0900

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,4 +1,5 @@
 setInterval(()=>{
   document.querySelectorAll("font").forEach(dev=>{dev.innerHTML="unchi"});
   document.querySelector("#pp>center>img").src="https://frame-illust.com/fi/wp-content/uploads/2018/02/unichi-makimaki.png";
+  document.querySelector("#change").href="http://abehiroshi.la.coocan.jp/";
 },10)
